e05938cffa1ca0a0763959b926545771d1a8c2a7
yang <136783693+taeseokyang@users.noreply.github.com>
2024-06-29 08:39:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import serial
+from pyubx2 import UBXReader
+
+stream = serial.Serial("/dev/ttyAMA2", baudrate=9600, timeout=50)
+ubr = UBXReader(stream)
+file =  open("gps_data.txt", "a")
+
+while True:
+	(raw_data, parsed_data) = ubr.read()
+	print(raw_data)
+	file.write(str(raw_data))
