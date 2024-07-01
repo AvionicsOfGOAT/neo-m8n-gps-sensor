50e2f180f50fe0c1cf12e36e98af8e1a7b2296aa
yang <136783693+taeseokyang@users.noreply.github.com>
2024-07-01 15:36:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ from pyubx2 import UBXReader
 stream = serial.Serial("/dev/ttyAMA2", baudrate=9600, timeout=50)
 ubr = UBXReader(stream)
 file =  open("gps_data.txt", "a")
-
 while True:
-	(raw_data, parsed_data) = ubr.read()
-	print(raw_data)
-	file.write(str(raw_data))
+    (raw_data, parsed_data) = ubr.read()
+    lines = str(raw_data).split('$')
+    gngll_data = ""
+    for line in lines:
+        if line.startswith('GNGLL'):
+            gngll_data = line
+            break
+    gngll_data = gngll_data.replace(',,', ',')
+    parts = gngll_data.split(',')
+
+    if len(parts) > 5:  # 유효성 확인
+        try:
+            latitude = float(parts[1][:2]) + float(parts[1][2:]) / 60
+            longitude = float(parts[3][:3]) + float(parts[3][3:]) / 60
+            file.write(f"{{ lat: {location['lat']}, lng: {location['lng']} }},\n")
+        except ValueError:
+            continue
